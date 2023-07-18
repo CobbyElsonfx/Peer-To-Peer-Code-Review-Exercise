e057ff319bc6c08f0ee1955f51cc56393acdc1fb
MR. ANDOH FRANCIS <andohfrancis9187@gmail.com>
2023-07-18 10:30:27 +0000

    prepared dir for code review

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name]. bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    assetModuleFilename: 'images/[name][ext]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
+      },
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
